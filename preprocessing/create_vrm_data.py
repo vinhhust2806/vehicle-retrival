604589ba92b831d8a64f3d935fafab619d6f6ca4
Quang-Vinh Nguyen <88282800+vinhhust2806@users.noreply.github.com>
2024-01-22 22:19:42 +0900

    Update create_vrm_data.py

--- preprocessing/create_vrm_data.py
+++ preprocessing/create_vrm_data.py
@@ -7,7 +7,7 @@ import pickle
 # generate video recognition model data
 def msvd_format_data():
     train_list, val_list, test_list = "", "", ""
-    path = "./vehicle-retrival/data/train-tracks.json"
+    path = "/vehicle-retrival/data/train_tracks.json"
     data = {}
     with open(path, "r") as f:
         data = json.load(f)
@@ -29,17 +29,17 @@ def msvd_format_data():
         else:
             train_list += str(vid) + "\n"
 
-    txt_path = "./vehicle-retrival/data/test-queries.json"
+    txt_path = "/vehicle-retrival/data/test_queries.json"
     txt_data = {}
     with open(txt_path, "r") as f:
         txt_data = json.load(f)
     print(len(txt_data))
 
-    video_path = "./vehicle-retrival/data/test-tracks.json"
+    video_path = "/vehicle-retrival/data/test_tracks.json"
     video_data = {}
     with open(video_path, "r") as f:
         video_data = json.load(f)
-    suffix = "/data"
+
     print(len(video_data))
     # assume the test text-video match by index
     for tid, vid in zip(txt_data, video_data):
@@ -53,20 +53,20 @@ def msvd_format_data():
         aicity_dict[vid] = new_info
         test_list += str(vid) + "\n"
         
-    with open('./vehicle-retrival/xclip/data/raw-captions.pkl', 'wb') as handle:
+    with open('/vehicle-retrival/xclip/data/raw-captions.pkl', 'wb') as handle:
         pickle.dump(aicity_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)    
     
-    with open('./vehicle-retrival/xclip/data/train_list.txt', 'w') as f:
+    with open('/vehicle-retrival/xclip/data/train_list.txt', 'w') as f:
         f.write(train_list)
-    with open('./vehicle-retrival/xclip/data/val_list.txt', 'w') as f:
+    with open('/vehicle-retrival/xclip/data/val_list.txt', 'w') as f:
         f.write(val_list)
-    with open('./vehicle-retrival/xclip/data/test_list.txt', 'w') as f:
+    with open('/vehicle-retrival/xclip/data/test_list.txt', 'w') as f:
         f.write(test_list)
         
         
 # generate video recognition model data with caption weight
 def msvd_format_data2():
-    path = "./vehicle-retrival/data/train-tracks.json"
+    path = "/vehicle-retrival/data/train_tracks.json"
     data = {}
     with open(path, "r") as f:
         data = json.load(f)
@@ -89,18 +89,16 @@ def msvd_format_data2():
                     new_info.append([nl_list, 0.3])
         aicity_dict[vid] = new_info
 
-    txt_path = "./vehicle-retrival/data/test-queries.json"
+    txt_path = "/vehicle-retrival/data/test_queries.json"
     txt_data = {}
     with open(txt_path, "r") as f:
         txt_data = json.load(f)
     print(len(txt_data))
 
-    video_path = "./vehicle-retrival/data/test-tracks.json"
+    video_path = "/vehicle-retrival/data/test_tracks.json"
     video_data = {}
     with open(video_path, "r") as f:
         video_data = json.load(f)
-    suffix = "/data"
-
     
     for tid, vid in zip(txt_data, video_data):
         info = txt_data[tid]
@@ -118,7 +116,7 @@ def msvd_format_data2():
                     new_info.append([nl_list, 0.3])
         aicity_dict[vid] = new_info
         
-    with open('./vehicle-retrival/xclip/data/raw-captions2.pkl', 'wb') as handle:
+    with open('/vehicle-retrival/xclip/data/raw-captions2.pkl', 'wb') as handle:
         pickle.dump(aicity_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)    
     
         
