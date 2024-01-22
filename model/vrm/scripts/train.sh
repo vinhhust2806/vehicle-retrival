cb318a8d18c387f41b3fed27838aba9ca11efcb1
Quang-Vinh Nguyen <88282800+vinhhust2806@users.noreply.github.com>
2024-01-22 18:11:19 +0900

    Update train.sh

--- model/vrm/scripts/train.sh
+++ model/vrm/scripts/train.sh
@@ -1,6 +1,6 @@
 # ViT-B/16
 job_name="train_vsm_vit16_test01" 
-DATA_PATH="C:"
+DATA_PATH="Vehicle-Retrival"
 python -m torch.distributed.launch --nproc_per_node=4 \
     train_vrm.py --do_train --num_thread_reader=8 \
     --epochs=50 --batch_size=40 --n_display=10 \
