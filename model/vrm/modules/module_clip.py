3e39ad07d247f349f8d2d237b1352631aa9df642
Quang-Vinh Nguyen <88282800+vinhhust2806@users.noreply.github.com>
2024-01-30 16:31:46 +0900

    Update module_clip.py

--- model/vrm/modules/module_clip.py
+++ model/vrm/modules/module_clip.py
@@ -22,6 +22,7 @@ _MODELS = {
     "RN50x16": "https://openaipublic.azureedge.net/clip/models/52378b407f34354e150460fe41077663dd5b39c54cd0bfd2b27167a4a06ec9aa/RN50x16.pt",
     "ViT-B/32": "https://openaipublic.azureedge.net/clip/models/40d365715913c9da98579312b702a82c18be219cc2a73407c4526f58eba950af/ViT-B-32.pt",
     "ViT-B/16": "https://openaipublic.azureedge.net/clip/models/5806e77cd80f8b59890b7e101eabd078d9fb84e6937f9e85e4ecb61988df416f/ViT-B-16.pt",
+    "ViT-L/14": "https://openaipublic.azureedge.net/clip/models/b8cca3fd41ae0c99ba7e8951adf17d267cdb84cd88be6f7c2e0eca1737a03836/ViT-L-14.pt",
 }
 _PT_NAME = {
     "RN50": "RN50.pt",
@@ -30,6 +31,7 @@ _PT_NAME = {
     "RN50x16": "RN50x16.pt",
     "ViT-B/32": "ViT-B-32.pt",
     "ViT-B/16": "ViT-B-16.pt",
+    "ViT-L/14": "ViT-L-14.pt",
 }
 
 def _download(url: str, root: str = os.path.expanduser("~/.cache/clip")):
